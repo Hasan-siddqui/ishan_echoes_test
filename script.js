aab83158f1dab2a4ed39b217dd2019b12bf6259e
Hasan <148314926+Hasan-siddqui@users.noreply.github.com>
2024-02-23 21:57:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+// Details start
+$(document).ready(function() {
+
+    var curPage = 1;
+    var numOfPages = $(".skw-page").length;
+    var animTime = 1000;
+    var scrolling = false;
+    var pgPrefix = ".skw-page-";
+  
+    function pagination() {
+      scrolling = true;
+  
+      $(pgPrefix + curPage).removeClass("inactive").addClass("active");
+      $(pgPrefix + (curPage - 1)).addClass("inactive");
+      $(pgPrefix + (curPage + 1)).removeClass("active");
+  
+      setTimeout(function() {
+        scrolling = false;
+      }, animTime);
+    };
+  
+    function navigateUp() {
+      if (curPage === 1) return;
+      curPage--;
+      pagination();
+    };
+  
+    function navigateDown() {
+      if (curPage === numOfPages) return;
+      curPage++;
+      pagination();
+    };
+  
+    $(document).on("mousewheel DOMMouseScroll", function(e) {
+      if (scrolling) return;
+      if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+        navigateUp();
+      } else { 
+        navigateDown();
+      }
+    });
+  
+    $(document).on("keydown", function(e) {
+      if (scrolling) return;
+      if (e.which === 38) {
+        navigateUp();
+      } else if (e.which === 40) {
+        navigateDown();
+      }
+    });
+  
+  });
+
+  // Details end
+// our work start
+const filterContainer = document.querySelector(".gallery-filter"),
+	galleryItems = document.querySelectorAll(".gallery-item");
+
+filterContainer.addEventListener("click", (event) => {
+	if (event.target.classList.contains("filter-item")) {
+		// deactivate existing active 'filter-item'
+		filterContainer.querySelector(".active").classList.remove("active");
+		// activate new 'filter-item'
+		event.target.classList.add("active");
+		const filterValue = event.target.getAttribute("data-filter");
+		galleryItems.forEach((item) => {
+			if (item.classList.contains(filterValue) || filterValue === 'all') {
+				item.classList.remove("hide");
+				item.classList.add("show");
+			}
+			else {
+				item.classList.remove("show");
+				item.classList.add("hide");
+			}
+		});
+	}
+});
+// our work end
